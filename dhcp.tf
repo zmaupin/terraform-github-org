033fdab51d7022b5a60f8fff7cf593d47a0fb2a2
Dan Webb <dan.webb@damacus.io>
2017-01-06 18:45:39 +0000

    Add DHCP members

---
+++ dhcp.tf
@@ -0,0 +1,23 @@
+resource "github_team" "dhcp_team" {
+  name        = "dhcp"
+  description = "DHCP Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "dhcp-maintainer-1" {
+    team_id = "${github_team.dhcp_team.id}"
+    username = "jmccann"
+    role = "maintainer"
+}
+
+resource "github_team_membership" "dhcp-maintainer-2" {
+    team_id = "${github_team.dhcp_team.id}"
+    username = "shortdudey123"
+    role = "maintainer"
+}
+
+resource "github_team_repository" "dhcp_repo" {
+  team_id    = "${github_team.dhcp.id}"
+  repository = "dhcp"
+  permission = "admin"
+}
