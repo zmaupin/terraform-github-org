9441d400ec43b7bc41ada72ee65586b0e387ec40
Dan Webb <dan.webb@damacus.io>
2017-01-07 18:25:40 +0000

    Adds Nano cookbook

---
+++ nano.tf
@@ -0,0 +1,17 @@
+resource "github_team" "nano_team" {
+  name        = "nano"
+  description = "HAProxy Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "nano-maintainer-1" {
+    team_id = "${github_team.nano_team.id}"
+    username = "tas50"
+    role = "member"
+}
+
+resource "github_team_repository" "nano_repo" {
+  team_id    = "${github_team.nano_team.id}"
+  repository = "nano"
+  permission = "admin"
+}
