ed9849b0a93a80510aa8b934954c95270aa659fb
Dan Webb <dan.webb@damacus.io>
2017-01-07 13:03:54 +0000

    Add BSDCPIO

---
+++ bsdcpio.tf
@@ -0,0 +1,17 @@
+resource "github_team" "bsdcpio_team" {
+  name        = "bsdcpio"
+  description = "BSDCPIO Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "bsdcpio-maintainer-1" {
+    team_id = "${github_team.bsdcpio_team.id}"
+    username = "josephholsten"
+    role = "member"
+}
+
+resource "github_team_repository" "bsdcpio_repo" {
+  team_id    = "${github_team.bsdcpio_team.id}"
+  repository = "bsdcpio"
+  permission = "admin"
+}
