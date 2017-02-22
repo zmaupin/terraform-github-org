30df78399ca160eb4ff97dbbaf45f776b9c52bfd
Dan Webb <dan.webb@damacus.io>
2017-02-22 17:45:48 +0000

    Add swap cookbook

---
+++ swap.tf
@@ -0,0 +1,17 @@
+resource "github_team" "swap" {
+  name        = "swap"
+  description = "Swap Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "swap-maintainer-1" {
+  team_id  = "${github_team.swap.id}"
+  username = "tas50"
+  role     = "member"
+}
+
+resource "github_team_repository" "swap_repo" {
+  team_id    = "${github_team.swap.id}"
+  repository = "swap"
+  permission = "admin"
+}
