70687f16be4dec06f18b57bab6cd3d6fbdac4d5e
Richard Shade <rshade@rightscale.com>
2017-03-10 07:39:47 -0600

    adding the filesystem cookbook

---
+++ filesystem.tf
@@ -0,0 +1,23 @@
+resource "github_team" "filesystem" {
+  name        = "filesystem"
+  description = "Ruby Build Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "filesystem-maintainer-1" {
+  team_id  = "${github_team.filesystem.id}"
+  username = "rshade"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "filesystem-maintainer-2" {
+  team_id  = "${github_team.filesystem.id}"
+  username = "tas50"
+  role     = "maintainer"
+}
+
+resource "github_team_repository" "filesystem_repo" {
+  team_id    = "${github_team.filesystem.id}"
+  repository = "filesystem"
+  permission = "admin"
+}
