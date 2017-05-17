88b3fd6b23ef693ba7613cdff551d9f53d9ee28d
Dan Webb <dan.webb@damacus.io>
2017-05-17 15:02:57 +0100

    rename repository`

--- website.tf
+++ website.tf
@@ -6,6 +6,6 @@ resource "github_team_repository" "website" {
 
 resource "github_team_repository" "website-sous-access" {
   team_id    = "${github_team.Chef_de_partie.id}"
-  repository = "sous-chefs.github.io"
+  repository = "website"
   permission = "push"
 }
