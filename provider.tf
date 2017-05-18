3bc0d9609dab7b77d12ed6fd8e8993faa63afe6b
Dan Webb <dan.webb@damacus.io>
2017-05-18 09:16:43 +0100

    Use "new" backend

--- provider.tf
+++ provider.tf
@@ -2,3 +2,9 @@ provider "github" {
   token        = "${var.github_token}"
   organization = "${var.github_organization}"
 }
+
+terraform {
+  backend "atlas" {
+    name         = "sous-chefs/terraform-github-org"
+  }
+}
