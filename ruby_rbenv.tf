54c3520c449d2a1bfbaacb30720e22ac0c9cb2b1
Tim Smith <tsmith@chef.io>
2018-07-17 22:13:54 -0700

    Rename
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- ruby_rbenv.tf
+++ ruby_rbenv.tf
@@ -11,7 +11,7 @@ resource "github_team" "ruby_rbenv" {
   privacy     = "closed"
 }
 
-resource "github_team_membership" "ruby_rbenv-maintainer-2" {
+resource "github_team_membership" "ruby_rbenv-maintainer-1" {
   team_id  = "${github_team.ruby_rbenv.id}"
   username = "tas50"
   role     = "maintainer"
