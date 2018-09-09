5d2fbe0ef5777accf3b0b89a8e4375cd516bf6c0
Tim Smith <tsmith@chef.io>
2018-09-08 20:40:38 -0700

    Move aptly out of the hw-cookbooks config
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- hw-cookbooks.tf
+++ hw-cookbooks.tf
@@ -1,10 +1,3 @@
-module "aptly" {
-  source         = "modules/repository"
-  name           = "aptly"
-  cookbook_team  = "${github_team.Chef_de_partie.id}"
-  chef_de_partie = "${github_team.Chef_de_partie.id}"
-}
-
 module "control_groups" {
   source         = "modules/repository"
   name           = "control_groups"
