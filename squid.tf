c6a772b326ed3473a2a8e9dfb33db89770798e0d
Xorima <4923914+Xorima@users.noreply.github.com>
2019-02-18 09:27:03 +0000

    squid (#142)
    * Set check to require delivery not lint

---
+++ squid.tf
@@ -0,0 +1,25 @@
+module "squid" {
+  source         = "modules/repository"
+  name           = "squid"
+  cookbook_team  = "${github_team.squid.id}"
+  chef_de_partie = "${github_team.Chef_de_partie.id}"
+  status_checks  = ["ci/circleci: delivery"]
+}
+
+resource "github_team" "squid" {
+  name        = "squid"
+  description = "squid Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "squid-maintainer-1" {
+  team_id  = "${github_team.squid.id}"
+  username = "tas50"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "squid-maintainer-2" {
+  team_id  = "${github_team.squid.id}"
+  username = "xorima"
+  role     = "member"
+}
