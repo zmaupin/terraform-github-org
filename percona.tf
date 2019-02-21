a9ea9d9acdb8fdd612889fbdf05ad19bd265ad9d
Dan Webb <dan.webb@damacus.io>
2019-02-21 14:02:19 +0000

    Fix checks on percona (#143)

--- percona.tf
+++ percona.tf
@@ -3,6 +3,7 @@ module "percona" {
   name           = "percona"
   cookbook_team  = "${github_team.percona.id}"
   chef_de_partie = "${github_team.Chef_de_partie.id}"
+  status_checks  = ["ci/circleci: delivery"]
 }
 
 resource "github_team" "percona" {
