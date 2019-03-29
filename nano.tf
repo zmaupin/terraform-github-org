2932a53465870f8660937f197e33dd128af3091b
Xorima <4923914+Xorima@users.noreply.github.com>
2019-03-29 16:21:44 +0000

    nano maintainer (#153)
    Add xorima to be a maintainer
    Update to use delivery status checks

--- nano.tf
+++ nano.tf
@@ -2,6 +2,7 @@ module "nano" {
   source        = "modules/repository"
   name          = "nano"
   cookbook_team = "${github_team.nano.id}"
+  status_checks = ["ci/circleci: delivery"]
 }
 
 resource "github_team" "nano" {
@@ -13,5 +14,11 @@ resource "github_team" "nano" {
 resource "github_team_membership" "nano-maintainer-1" {
   team_id  = "${github_team.nano.id}"
   username = "tas50"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "nano-maintainer-2" {
+  team_id  = "${github_team.nano.id}"
+  username = "Xorima"
   role     = "maintainer"
 }
