f966176d08c12cf61a45fd7d1a920ba11189a6e0
Xorima <4923914+Xorima@users.noreply.github.com>
2019-04-02 22:04:42 +0100

    Add Keepalived (#154)

---
+++ keepalived.tf
@@ -0,0 +1,24 @@
+module "keepalived" {
+  source                     = "modules/repository"
+  name                       = "keepalived"
+  cookbook_team              = "${github_team.keepalived.id}"
+  status_checks              = ["ci/circleci: delivery"]
+}
+
+resource "github_team" "keepalived" {
+  name        = "keepalived"
+  description = "keepalived Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "keepalived-maintainer-1" {
+  team_id  = "${github_team.keepalived.id}"
+  username = "Xorima"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "keepalived-maintainer-2" {
+  team_id  = "${github_team.keepalived.id}"
+  username = "tas50"
+  role     = "member"
+}
