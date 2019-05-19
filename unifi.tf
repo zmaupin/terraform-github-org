eb62d96c98fbdf3408f2e5dc32b65d9b6c58923d
Xorima <4923914+Xorima@users.noreply.github.com>
2019-05-19 11:31:02 +0100

    Unifi circleci 2.0 (#168)

--- unifi.tf
+++ unifi.tf
@@ -2,6 +2,7 @@ module "unifi" {
   source        = "modules/repository"
   name          = "unifi"
   cookbook_team = "${github_team.unifi.id}"
+  status_checks = ["ci/circleci: delivery"]
 }
 
 resource "github_team" "unifi" {
@@ -13,5 +14,11 @@ resource "github_team" "unifi" {
 resource "github_team_membership" "unifi-maintainer-1" {
   team_id  = "${github_team.unifi.id}"
   username = "tas50"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "unifi-maintainer-2" {
+  team_id  = "${github_team.unifi.id}"
+  username = "Xorima"
   role     = "maintainer"
 }
