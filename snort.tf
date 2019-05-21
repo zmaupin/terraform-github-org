17ca4d7c708501adb5c3df7ce8d5a0b087eed1d7
Xorima <4923914+Xorima@users.noreply.github.com>
2019-05-21 08:23:53 +0100

    snort circle 2.0 (#171)

--- snort.tf
+++ snort.tf
@@ -2,6 +2,7 @@ module "snort" {
   source        = "modules/repository"
   name          = "snort"
   cookbook_team = "${github_team.snort.id}"
+  status_checks = ["ci/circleci: delivery"]
 }
 
 resource "github_team" "snort" {
@@ -13,5 +14,11 @@ resource "github_team" "snort" {
 resource "github_team_membership" "snort-maintainer-1" {
   team_id  = "${github_team.snort.id}"
   username = "tas50"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "snort-maintainer-2" {
+  team_id  = "${github_team.snort.id}"
+  username = "damacus"
   role     = "maintainer"
 }
